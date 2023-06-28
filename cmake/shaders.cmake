275a3925e0780b76fe3bfd9ae881b05cad6ff6bb
Peter <pm@trayportgermany.com>
2023-06-28 15:01:01 +0100

    Shaders

---
+++ cmake/shaders.cmake
@@ -0,0 +1,54 @@
+function(add_shader TARGET SHADER)
+    if (${GLSLC_PATH} STREQUAL "")
+        message(FATAL_ERROR "GLSLC_PATH is not set. This needs to be defined to compile shaders.")
+    endif()
+
+    # We will make this require VULKAN_PATH that is defined from including vulkan.cmake
+    # Here we define the executables we require to build. Might need to add something like
+    # `if (${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "AMD64")` to support 32bit but we can
+    # do that later
+    set(GLSLC ${GLSLC_PATH}/glslc.exe)
+
+    # Check the file extension
+    get_filename_component(SHADER_EXT ${SHADER} LAST_EXT)
+    string(TOLOWER ${SHADER_EXT} SHADER_EXT_LOWER)
+    set(ALLOWED_SHADER_EXTENSIONS
+            ".vert" # vertex shader
+            ".frag" # fragment shader
+            ".geom" # geometry shader
+            ".tesc" # tessellation control shader
+            ".tese" # tessellation evaluation shader
+            ".comp" # compute shader
+    )
+    if (NOT ("${SHADER_EXT_LOWER}" IN_LIST ALLOWED_SHADER_EXTENSIONS))
+        message(FATAL_ERROR "Invalid shader extension for file ${SHADER}: ${SHADER_EXT}")
+    endif()
+
+    set(SHADER_OUTPUT_NAME ${SHADER})
+#    # If the shader has a .glsl file extension then remove it
+#    get_filename_component(SHADER_EXT ${SHADER} LAST_EXT)
+#    string(TOLOWER ${SHADER_EXT} SHADER_EXT_LOWER)
+#    if(SHADER_EXT_LOWER STREQUAL ".glsl")
+#        message(DEBUG "Stripping glsl file extension")
+#        get_filename_component(SHADER_OUTPUT_NAME ${SHADER} NAME_WLE)
+#    endif()
+
+    set(current_shader_path ${CMAKE_CURRENT_SOURCE_DIR}/${SHADER})
+    set(current_output_path ${CMAKE_CURRENT_BINARY_DIR}/${SHADER_OUTPUT_NAME}.spv)
+
+    message("Compiling ${current_shader_path} -> ${current_output_path}")
+    # Add a custom command to compile GLSL to SPIR-V.
+    get_filename_component(current_output_dir ${current_output_path} DIRECTORY)
+    file(MAKE_DIRECTORY ${current_output_dir})
+
+    add_custom_command(
+            OUTPUT ${current_output_path}
+            COMMAND ${GLSLC} -o ${current_output_path} ${current_shader_path}
+            DEPENDS ${current_shader_path}
+            IMPLICIT_DEPENDS CXX ${current_shader_path}
+            VERBATIM)
+
+    # Make sure our build depends on this output.
+    set_source_files_properties(${current_output_path} PROPERTIES GENERATED TRUE)
+    target_sources(${TARGET} PRIVATE ${current_output_path})
+endfunction(add_shader)
--- src/opengl/Shader.cpp
+++ src/opengl/Shader.cpp
@@ -1,1 +1,187 @@
 #include "Shader.hpp"
+
+#include <glad/glad.h>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <fstream>
+#include <iostream>
+
+namespace gfxlib
+{
+    Shader::Shader(const std::filesystem::path &vertexShader, std::filesystem::path &fragmentShader)
+            : m_vertexPath(vertexShader), m_fragmentPath(fragmentShader)
+    {
+        m_vertexBinarySrc = loadShader(vertexShader);
+        m_fragmentBinarySrc = loadShader(fragmentShader);
+    }
+
+    Shader::~Shader()
+    {
+        glDeleteProgram(m_id);
+    }
+
+    std::vector<char> Shader::loadShader(const std::filesystem::path &shader)
+    {
+        std::ifstream shaderFile;
+        shaderFile.open(shader, std::ios::binary | std::ios::ate);
+        if (shaderFile.is_open())
+        {
+            size_t size = shaderFile.tellg();
+            shaderFile.seekg(0, std::ios::beg);
+            char* buffer = new char[size];
+            shaderFile.read(buffer, size);
+            std::vector<char> data(buffer, buffer + size);
+            return data;
+        }
+        else
+        {
+            std::cerr << "Could not open \"" << shader << "\"" << std::endl;
+            return std::vector<char>();
+        }
+    }
+
+    void Shader::createProgram()
+    {
+        GLuint program = glCreateProgram();
+
+        std::vector<GLuint> shaderIds;
+
+        {
+            auto spirv = m_vertexBinarySrc;
+            GLuint shaderID = shaderIds.emplace_back(glCreateShader(GL_VERTEX_SHADER));
+            glShaderBinary(1, &shaderID, GL_SHADER_BINARY_FORMAT_SPIR_V, spirv.data(), spirv.size() * sizeof(uint32_t));
+            glSpecializeShader(shaderID, "main", 0, nullptr, nullptr);
+            glAttachShader(program, shaderID);
+        }
+
+        {
+            auto spirv = m_fragmentBinarySrc;
+            GLuint shaderID = shaderIds.emplace_back(glCreateShader(GL_FRAGMENT_SHADER));
+            glShaderBinary(1, &shaderID, GL_SHADER_BINARY_FORMAT_SPIR_V, spirv.data(), spirv.size() * sizeof(uint32_t));
+            glSpecializeShader(shaderID, "main", 0, nullptr, nullptr);
+            glAttachShader(program, shaderID);
+        }
+
+        glLinkProgram(program);
+
+        GLint isLinked;
+        glGetProgramiv(program, GL_LINK_STATUS, &isLinked);
+        if (isLinked == GL_FALSE)
+        {
+            GLint maxLength;
+            glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
+
+            std::vector<GLchar> infoLog(maxLength);
+            glGetProgramInfoLog(program, maxLength, &maxLength, infoLog.data());
+            std::cerr << "Failed to link Shader: " << m_vertexPath << "/" << m_fragmentPath << ". " << infoLog.data() << std::endl;
+
+            glDeleteProgram(program);
+
+            for (auto id : shaderIds)
+            {
+                glDeleteShader(id);
+            }
+        }
+
+        for (auto id : shaderIds)
+        {
+            glDetachShader(program, id);
+            glDeleteShader(id);
+        }
+
+        m_id = program;
+    }
+
+    void Shader::bind() const
+    {
+        glUseProgram(m_id);
+    }
+
+    void Shader::unbind() const
+    {
+        glUseProgram(0);
+    }
+
+    void Shader::setInt(const std::string &name, int value)
+    {
+        uploadUniformInt(name, value);
+    }
+
+    void Shader::setIntArray(const std::string &name, int *values, uint32_t count)
+    {
+        uploadUniformIntArray(name, values, count);
+    }
+
+    void Shader::setFloat(const std::string &name, float value)
+    {
+        uploadUniformFloat(name, value);
+    }
+
+    void Shader::setFloat2(const std::string &name, const glm::vec2 &value)
+    {
+        uploadUniformFloat2(name, value);
+    }
+
+    void Shader::setFloat3(const std::string &name, const glm::vec3 &value)
+    {
+        uploadUniformFloat3(name, value);
+    }
+
+    void Shader::setFloat4(const std::string &name, const glm::vec4 &value)
+    {
+        uploadUniformFloat4(name, value);
+    }
+
+    void Shader::setMat4(const std::string &name, const glm::mat4 &value)
+    {
+        uploadUniformMat4(name, value);
+    }
+
+    void Shader::uploadUniformInt(const std::string &name, int value)
+    {
+        GLint location = glGetUniformLocation(m_id, name.c_str());
+        glUniform1i(location, value);
+    }
+
+    void Shader::uploadUniformIntArray(const std::string &name, int *values, uint32_t count)
+    {
+        GLint location = glGetUniformLocation(m_id, name.c_str());
+        glUniform1iv(location, count, values);
+    }
+
+    void Shader::uploadUniformFloat(const std::string &name, float value)
+    {
+        GLint location = glGetUniformLocation(m_id, name.c_str());
+        glUniform1f(location, value);
+    }
+
+    void Shader::uploadUniformFloat2(const std::string &name, const glm::vec2 &value)
+    {
+        GLint location = glGetUniformLocation(m_id, name.c_str());
+        glUniform2f(location, value.x, value.y);
+    }
+
+    void Shader::uploadUniformFloat3(const std::string &name, const glm::vec3 &value)
+    {
+        GLint location = glGetUniformLocation(m_id, name.c_str());
+        glUniform3f(location, value.x, value.y, value.z);
+    }
+
+    void Shader::uploadUniformFloat4(const std::string &name, const glm::vec4 &value)
+    {
+        GLint location = glGetUniformLocation(m_id, name.c_str());
+        glUniform4f(location, value.x, value.y, value.z, value.w);
+    }
+
+    void Shader::uploadUniformMat3(const std::string &name, const glm::mat3 &matrix)
+    {
+        GLint location = glGetUniformLocation(m_id, name.c_str());
+        glUniformMatrix3fv(location, 1, GL_FALSE, glm::value_ptr(matrix));
+    }
+
+    void Shader::uploadUniformMat4(const std::string &name, const glm::mat4 &matrix)
+    {
+        GLint location = glGetUniformLocation(m_id, name.c_str());
+        glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(matrix));
+    }
+}
